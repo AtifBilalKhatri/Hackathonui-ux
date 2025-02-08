f1319870d7b2fc36258f2885b51d921283d21b1e
Atif Bilal Khatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2025-02-08 15:59:14 +0500

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.sanity.io',
+          port: '',
+          pathname: '/**', // Allow all paths (recommended)
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
